53caf10c63cb3e1dec1b07a7b2db91f0bd12a73b
cwelland1 <75807390+cwelland1@users.noreply.github.com>
2021-10-15 10:05:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 function onRequest(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World DOED');
+    response.write('Hello World DOED TEST');
     response.end();
 }
 
